0b8630cbf8cc8277cc2c9355e69ea3d9b6550cf6
michellevit <mvflandin@gmail.com>
2022-08-04 17:55:26 -0700

    add newest version of script and accompanying files to branch, and delete old non-functional files

---
+++ run_amazon_script_1.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python "C:\Users\Michelle Flandin\Documents\Amazon_Program\main.py"
+
+pause
---
+++ run_amazon_script_2.bat
@@ -0,0 +1,5 @@
+@echo off
+
+python "C:\Users\Michelle Flandin\Documents\Amazon_Program\amazon_script_2.py"
+
+pause
