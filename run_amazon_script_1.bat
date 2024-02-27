fc205adbdb273118844b53093cb7b2b89b494a5d
michellevit <mvflandin@gmail.com>
2024-02-27 11:47:00 -0800

    Remove @rem statement

--- run_amazon_script_1.bat
+++ run_amazon_script_1.bat
@@ -4,4 +4,3 @@ call C:\Users\Michelle\.virtualenvs\Amazon-Checker-Virtual-Env\Scripts\activate
 python "C:\Users\Michelle\Documents\Coding_Projects\Amazon-Inventory-Checker\main.py"
 
 pause
-@REM exit
